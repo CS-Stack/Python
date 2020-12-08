02304711bc279b97081cf4ad7a0c9ccb70259990
Atharva Chandwadkar <chandwadkar28@gmail.com>
2020-12-08 22:57:08 +0530

    added while loop python program

---
+++ Loops/while.py
@@ -0,0 +1,16 @@
+#Sample python program to represent While loop
+# To take input from the user,
+# n = int(input("Enter n: "))
+
+n = 10
+
+# initialize sum and counter
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum + i
+    i = i+1    # update counter
+
+# print the sum
+print("The sum is", sum)
