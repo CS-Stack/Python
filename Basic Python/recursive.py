2395d910d36abe7143e808b1640203ae635525ca
Atharva Chandwadkar <chandwadkar28@gmail.com>
2020-12-12 14:28:22 +0530

    Added Python Program to represent recursive function to find the factorial of an integer

---
+++ Basic Python/recursive.py
@@ -0,0 +1,11 @@
+#Python Program to represent recursive function to find the factorial of an integer
+def factorial(x):
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = 3
+print("The factorial of", num, "is", factorial(num))
