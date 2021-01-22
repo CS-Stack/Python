424c7af1eb718ff09d669f00997eaa3e78b2914e
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-01-22 19:56:24 +0530

    Create class.py

---
+++ Python_OOP/class.py
@@ -0,0 +1,17 @@
+#Simple Class Creation in Python
+class Person:
+    "This is a person class"
+    age = 10
+
+    def greet(self):
+        print('Hello')
+
+
+# Output: 10
+print(Person.age)
+
+# Output: <function Person.greet>
+print(Person.greet)
+
+# Output: 'This is my second class'
+print(Person.__doc__)
